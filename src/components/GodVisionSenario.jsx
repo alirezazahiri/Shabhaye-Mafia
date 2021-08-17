618f0c1af59d36d15c4219644144ec17904b8858
Alireza <itsalireza.zr@gmail.com>
2021-08-17 11:52:06 +0430

    Searching Feature is Now available in GodVision and Players Section :fire:

--- src/components/GodVisionSenario.jsx
+++ src/components/GodVisionSenario.jsx
@@ -1,20 +1,57 @@
-import React from 'react'
-import Container from './common/Container'
-import Card from './common/Card'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import Container from "./common/Container";
+import Card from "./common/Card";
+import styled from "styled-components";
 
-const GodVisionSenario = () => {
-    const characters = JSON.parse(localStorage.getItem('characters'))
-    const players = JSON.parse(localStorage.getItem('players'))
-    const names = JSON.parse(localStorage.getItem('names'))
-    const n_p = JSON.parse(localStorage.getItem('n_p'))
+class GodVisionSenario extends Component {
+  state = {
+    characters: JSON.parse(localStorage.getItem("characters")),
+    players: JSON.parse(localStorage.getItem("players")),
+    names: JSON.parse(localStorage.getItem("names")),
+    n_p: JSON.parse(localStorage.getItem("n_p")),
+    fields: {
+      player: "",
+    },
+  };
 
-    return (  
-        <Container>
-            <Title>اتاق راوی</Title>
-            {players.map(player => {
-                const item = characters[names.indexOf(n_p[player])][n_p[player]]
-                return <Card 
+  handleChange = (e) => {
+    const name = e.target.name;
+    const changeFields = this.state.fields;
+    changeFields[name] = e.target.value;
+    this.setState({ fields: changeFields });
+  };
+  render() {
+    const { characters, players, names, n_p } = { ...this.state };
+    return (
+      <Container>
+        <Input
+          type="text"
+          className="block w-full text-white p-3 rounded mb-4"
+          name="player"
+          placeholder="... نقش یا بازیکن مورد نظر را جستجو کنید"
+          id="player"
+          onChange={this.handleChange}
+        />
+        <Title>اتاق راوی</Title>
+        {players
+          .filter((player) => {
+            const char = characters[names.indexOf(n_p[player])][n_p[player]];
+            return (
+              player
+                .trim()
+                .toLowerCase()
+                .includes(this.state.fields.player.trim().toLowerCase()) ||
+              char
+                .title
+                .trim()
+                .toLowerCase()
+                .includes(this.state.fields.player.trim().toLowerCase())
+            );
+          })
+          .map((player) => {
+            const item = characters[names.indexOf(n_p[player])][n_p[player]];
+            return (
+              <Card
                 key={player}
                 p_name={player}
                 title={item.title}
@@ -22,10 +59,12 @@ const GodVisionSenario = () => {
                 type={item.type}
                 icon={item.icon}
                 have_status={true}
-                />
-            })}
-        </Container>
+              />
+            );
+          })}
+      </Container>
     );
+  }
 }
 
 const Title = styled.h1`
@@ -36,7 +75,15 @@ const Title = styled.h1`
   font-size: 30px;
   font-weight: 900;
   letter-spacing: 1px;
-  font-family: 'Cairo', sans-serif;
+  font-family: "Cairo", sans-serif;
 `;
- 
+
+const Input = styled.input`
+  margin-top: 10px;
+  border: none;
+  background: rgba(0, 0, 0, 0.5);
+  font-family: "Cairo", sans-serif;
+  text-align: center;
+`;
+
 export default GodVisionSenario;
--- src/components/Senario.jsx
+++ src/components/Senario.jsx
@@ -1,31 +1,60 @@
 import React, { Component } from "react";
 import Card from "./common/Card";
 import Container from "./common/Container";
-import characters, {names} from "../utils/characters";
-import styled from 'styled-components'
+import characters, { names } from "../utils/characters";
+import styled from "styled-components";
 
 class Senario extends Component {
-  state = {};
+  state = {
+    fields: {
+      role: "",
+    },
+  };
 
   getCharacter = (idx) => {
-      return characters[idx][names[idx]]
-  }
+    return characters[idx][names[idx]];
+  };
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const changeFields = this.state.fields;
+    changeFields[name] = e.target.value;
+    this.setState({ fields: changeFields });
+  };
+
   render() {
     return (
       <Container>
+        <Input
+          type="text"
+          className="block w-full text-white p-3 rounded mb-4"
+          name="role"
+          placeholder="... نقش مورد نظر را جستجو کنید"
+          id="role"
+          onChange={this.handleChange}
+        />
         <Title>سناریو ها</Title>
-        {characters.map((character, index) => {
-            const char = this.getCharacter(index)
-          return (
-            <Card
-              key={names[index]}
-              title={char.title}
-              description={char.description}
-              icon={char.icon}
-              type={char.type}
-            />
-          );
-        })}
+        {characters
+          .filter((character) => {
+            const idx = characters.indexOf(character)
+            return character[names[idx]].title
+              .trim()
+              .toLowerCase()
+              .includes(this.state.fields.role.trim().toLowerCase());
+          })
+          .map((character, index) => {
+            const idx = characters.indexOf(character)
+            const char = this.getCharacter(idx);
+            return (
+              <Card
+                key={names[idx]}
+                title={char.title}
+                description={char.description}
+                icon={char.icon}
+                type={char.type}
+              />
+            );
+          })}
       </Container>
     );
   }
@@ -39,7 +68,15 @@ const Title = styled.h1`
   font-size: 30px;
   font-weight: 900;
   letter-spacing: 1px;
-  font-family: 'Cairo', sans-serif;
+  font-family: "Cairo", sans-serif;
+`;
+
+const Input = styled.input`
+  margin-top: 10px;
+  border: none;
+  background: rgba(0, 0, 0, 0.5);
+  font-family: "Cairo", sans-serif;
+  text-align: center;
 `;
 
 export default Senario;
